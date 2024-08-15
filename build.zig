fe46ae5692eb51c71e9c7ab1078138439a875ae8
lklein <lars.klein@epfl.ch>
2024-08-15 18:52:56 +0200

    drawing a devanagari character

--- build.zig
+++ build.zig
@@ -4,16 +4,17 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raygui = raylib_dep.module("raygui");
+    const raylib_artifact = raylib_dep.artifact("raylib");
 
     const lib = b.addStaticLibrary(.{
         .name = "zigbuzz",
@@ -35,6 +36,10 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raygui", raygui);
 
     // Use mach-freetype
     const mach_freetype_dep = b.dependency("mach_freetype", .{
--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,19 @@ const freetype = @import("mach-freetype");
 const harfbuzz = @import("mach-harfbuzz");
 const font_assets = @import("font-assets");
 
+const rl = @import("raylib");
+const rg = @import("raygui");
+
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer gpa.deinit();
-    // const allocator = gpa.allocator();
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 400;
+
+    rl.initWindow(screenWidth, screenHeight, "devanagari text");
+    defer rl.closeWindow(); // Close window and OpenGL context
+
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     const lib = try freetype.Library.init();
     defer lib.deinit();
@@ -17,7 +26,10 @@ pub fn main() !void {
 
     //try face.loadChar('A', .{ .render = true });
     try face.loadChar('आ', .{ .render = true });
-    const bitmap = face.glyph().bitmap();
+    var bitmap = face.glyph().bitmap();
+
+    std.debug.print("bitmap width: {d}\n", .{bitmap.width()});
+    std.debug.print("bitmap rows: {d}\n", .{bitmap.rows()});
 
     var i: usize = 0;
     while (i < bitmap.rows()) : (i += 1) {
@@ -31,5 +43,31 @@ pub fn main() !void {
             std.debug.print("{c}", .{char});
         }
         std.debug.print("\n", .{});
+    }
+
+    // Create an image from the bitmap data
+    var image: rl.Image = undefined;
+    image.width = 50;
+    image.height = 36;
+    image.mipmaps = 1;
+    image.format = rl.PixelFormat.pixelformat_uncompressed_grayscale;
+    // const ptr: *anyopaque = bitmap.buffer().?.ptr;
+    image.data = @constCast(bitmap.buffer().?.ptr);
+
+    // Load the image into a texture
+    const texture: rl.Texture2D = rl.loadTextureFromImage(image);
+
+    // Main game loop
+    while (!rl.windowShouldClose()) {
+        // Update
+        //----------------------------------------------------------------------------------
+
+        // Draw
+
+        rl.beginDrawing();
+        defer rl.endDrawing();
+        rl.clearBackground(rl.Color.white);
+
+        rl.drawTexture(texture, 400, 200, rl.Color.white);
     }
 }
