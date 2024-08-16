5e692134a296f10040f6429f8514774903c625cd
lklein <lars.klein@epfl.ch>
2024-08-16 12:00:20 +0200

    formatting, better comments

--- src/main.zig
+++ src/main.zig
@@ -7,21 +7,21 @@ const rl = @import("raylib");
 const rg = @import("raygui");
 
 pub fn main() !void {
-    const screenWidth = 400;
-    const screenHeight = 200;
+    // raylib setup
+    const screenWidth = 800;
+    const screenHeight = 400;
     rl.initWindow(screenWidth, screenHeight, "devanagari text");
-    defer rl.closeWindow(); // Close window and OpenGL context
-    rl.setTargetFPS(1); // Set our game to run at 60 frames-per-second
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
 
+    // Normally I use the GPA allocator, but it seems incompatible with raylib.
+    // See further below for a comment on this issue.
     // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     // const allocator = gpa.allocator();
     // defer {
     //     _ = gpa.deinit();
     // }
     const allocator = std.heap.c_allocator;
-
-    const lib = try freetype.Library.init();
-    defer lib.deinit();
 
     const text = "मेरा नाम लार्स है";
     //const text = "मेर";
@@ -33,7 +33,11 @@ pub fn main() !void {
     hb_buffer.addUTF8(text, 0, text.len);
 
     // set up freetype face, convert to harfbuzz face, and shape the buffer
+    const lib = try freetype.Library.init();
+    defer lib.deinit();
     const ft_face = try lib.createFaceMemory(@embedFile("NotoSerifDevanagari_Condensed-Bold.ttf"), 0);
+
+    // ToDo: it's not clear to me how to set the font size correctly
     try ft_face.setCharSize(100 * 50, 0, 50, 0);
     const hb_face = harfbuzz.Face.fromFreetypeFace(ft_face);
     const hb_font = harfbuzz.Font.init(hb_face);
@@ -44,15 +48,24 @@ pub fn main() !void {
     const glyphPositions = hb_buffer.getGlyphPositions() orelse return;
 
     while (!rl.windowShouldClose()) {
+
+        // some raylib boilerplate
+        // the blendMode is important:
+        // harfbuzz and freetype ultimately give me bitmaps and positions
+        // the positions overlap
+        // if I'm drawing white text on a black background, then I can just use blend_additive to draw the glyphs
+        // ultimately I would like to get black text on white background, but I think that will require a more complex setup
         rl.beginDrawing();
         defer rl.endDrawing();
+        rl.clearBackground(rl.Color.black);
+        rl.beginBlendMode(rl.BlendMode.blend_additive);
+        defer rl.endBlendMode();
 
-        rl.clearBackground(rl.Color.white);
+        // start position for the text
+        var pos_x: i32 = 100;
+        var pos_y: i32 = 100;
 
         for (glyphInfos, glyphPositions) |info, pos| {
-            // this output looks ok-ish
-            // except that the offsets and advances in pos are quite large
-            // it's not clear to me how to interpret them
             std.debug.print("codepoint: {d}\n", .{info.codepoint});
             std.debug.print("{any}\n", .{pos});
 
@@ -60,7 +73,6 @@ pub fn main() !void {
             try ft_face.loadGlyph(info.codepoint, .{ .render = true });
             const glyph = ft_face.glyph();
             const bm = glyph.bitmap();
-
             std.debug.print("bitmap: {d}x{d}\n", .{ bm.width(), bm.rows() });
 
             // create an image from the bitmap data
@@ -83,22 +95,22 @@ pub fn main() !void {
             // does raylib free some texture buffer itself?
             // I'm not using raylib unloadImage or unloadTexture
             // to me this looks like a big memory leak
-            // but: If I don't use the c_allocator, I get a "pointer wasn't allocated" error
-            // so I guess raylib is trying to free this buffer and if it was allocated with a different buffer, then it fails to do so
+            // but: If I don't use the c_allocator, I get a "freed pointer wasn't allocated" error
+            // so I guess raylib is trying to free this buffer and if it was allocated with a different allocator, then it fails to do so
             //defer allocator.free(bitmapBuffer);
             image.data = bitmapBuffer.ptr;
 
-            // by default it's white text on black background
-            // I'd like to draw black on white
-            image.invert();
-
             // to draw, we need to convert the image into a texture
             const texture: rl.Texture2D = rl.loadTextureFromImage(image);
-            const center_x = 200;
-            const center_y = 100;
-            // ToDo: the positioning is off
-            // the offsets are much too large, and I've not yet figured out what to do with pos.advance
-            rl.drawTexture(texture, center_x + pos.x_offset, center_y + pos.y_offset, rl.Color.white);
+
+            // ToDo: the values for advance are much too high
+            // if I apply them, the textures are drawn offscreen
+            // pos_x += pos.x_advance;
+            // pos_y += pos.y_advance;
+            pos_x += 40;
+            pos_y += 0;
+
+            rl.drawTexture(texture, pos_x + pos.x_offset, pos_y + pos.y_offset, rl.Color.white);
         }
     }
 }
