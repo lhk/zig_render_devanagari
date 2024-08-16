04066423759bbfbc694972332d101248c15b71a3
lklein <lars.klein@epfl.ch>
2024-08-16 09:30:23 +0200

    setting up text rendering

--- src/main.zig
+++ src/main.zig
@@ -9,8 +9,8 @@ const rg = @import("raygui");
 pub fn main() !void {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 400;
+    const screenWidth = 200;
+    const screenHeight = 100;
 
     rl.initWindow(screenWidth, screenHeight, "devanagari text");
     defer rl.closeWindow(); // Close window and OpenGL context
@@ -20,13 +20,36 @@ pub fn main() !void {
     const lib = try freetype.Library.init();
     defer lib.deinit();
 
-    //const face = try lib.createFaceMemory(font_assets.fira_sans_regular_ttf, 0);
-    const face = try lib.createFaceMemory(@embedFile("NotoSerifDevanagari_Condensed-Bold.ttf"), 0);
-    try face.setCharSize(100 * 50, 0, 50, 0);
+    //const ft_face = try lib.createFaceMemory(font_assets.fira_sans_regular_ttf, 0);
 
-    //try face.loadChar('A', .{ .render = true });
-    try face.loadChar('आ', .{ .render = true });
-    var bitmap = face.glyph().bitmap();
+    const text = "मेरा नाम लार्स है";
+
+    // set up harfbuzz buffer, feed it the Devanagari text
+    var hb_buffer = harfbuzz.Buffer.init() orelse return;
+    hb_buffer.setDirection(harfbuzz.Direction.ltr);
+    hb_buffer.setScript(harfbuzz.Script.devanagari);
+    hb_buffer.addUTF8(text, 0, text.len);
+
+    // set up freetype face, convert to harfbuzz face, and shape the buffer
+    const ft_face = try lib.createFaceMemory(@embedFile("NotoSerifDevanagari_Condensed-Bold.ttf"), 0);
+    try ft_face.setCharSize(100 * 50, 0, 50, 0);
+    const hb_face = harfbuzz.Face.fromFreetypeFace(ft_face);
+    const hb_font = harfbuzz.Font.init(hb_face);
+    hb_font.shape(hb_buffer, null);
+
+    // glyph infos and positions, that's what we need to render the text
+    const glyphInfos = hb_buffer.getGlyphInfos();
+    const glyphPositions = hb_buffer.getGlyphPositions() orelse return;
+
+    for (glyphInfos, glyphPositions) |info, pos| {
+        std.debug.print("codepoint: {d}\n", .{info.codepoint});
+        std.debug.print("{any}\n", .{pos});
+        try ft_face.loadGlyph(info.codepoint, .{ .render = true });
+    }
+
+    //try ft_face.loadChar('A', .{ .render = true });
+    try ft_face.loadChar('आ', .{ .render = true });
+    var bitmap = ft_face.glyph().bitmap();
 
     std.debug.print("bitmap width: {d}\n", .{bitmap.width()});
     std.debug.print("bitmap rows: {d}\n", .{bitmap.rows()});
@@ -68,6 +91,6 @@ pub fn main() !void {
         defer rl.endDrawing();
         rl.clearBackground(rl.Color.white);
 
-        rl.drawTexture(texture, 400, 200, rl.Color.white);
+        rl.drawTexture(texture, 50, 25, rl.Color.white);
     }
 }
