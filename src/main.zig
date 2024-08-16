fe7a9b574f8a9b9ca0bea8306283f167f56a81af
lklein <lars.klein@epfl.ch>
2024-08-16 11:15:12 +0200

    trying to format a longer text

--- src/main.zig
+++ src/main.zig
@@ -7,22 +7,24 @@ const rl = @import("raylib");
 const rg = @import("raygui");
 
 pub fn main() !void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const screenWidth = 200;
-    const screenHeight = 100;
-
+    const screenWidth = 400;
+    const screenHeight = 200;
     rl.initWindow(screenWidth, screenHeight, "devanagari text");
     defer rl.closeWindow(); // Close window and OpenGL context
+    rl.setTargetFPS(1); // Set our game to run at 60 frames-per-second
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator = gpa.allocator();
+    // defer {
+    //     _ = gpa.deinit();
+    // }
+    const allocator = std.heap.c_allocator;
 
     const lib = try freetype.Library.init();
     defer lib.deinit();
 
-    //const ft_face = try lib.createFaceMemory(font_assets.fira_sans_regular_ttf, 0);
-
     const text = "मेरा नाम लार्स है";
+    //const text = "मेर";
 
     // set up harfbuzz buffer, feed it the Devanagari text
     var hb_buffer = harfbuzz.Buffer.init() orelse return;
@@ -41,56 +43,62 @@ pub fn main() !void {
     const glyphInfos = hb_buffer.getGlyphInfos();
     const glyphPositions = hb_buffer.getGlyphPositions() orelse return;
 
-    for (glyphInfos, glyphPositions) |info, pos| {
-        std.debug.print("codepoint: {d}\n", .{info.codepoint});
-        std.debug.print("{any}\n", .{pos});
-        try ft_face.loadGlyph(info.codepoint, .{ .render = true });
-    }
-
-    //try ft_face.loadChar('A', .{ .render = true });
-    try ft_face.loadChar('आ', .{ .render = true });
-    var bitmap = ft_face.glyph().bitmap();
-
-    std.debug.print("bitmap width: {d}\n", .{bitmap.width()});
-    std.debug.print("bitmap rows: {d}\n", .{bitmap.rows()});
-
-    var i: usize = 0;
-    while (i < bitmap.rows()) : (i += 1) {
-        var j: usize = 0;
-        while (j < bitmap.width()) : (j += 1) {
-            const char: u8 = switch (bitmap.buffer().?[i * bitmap.width() + j]) {
-                0 => ' ',
-                1...128 => ';',
-                else => '#',
-            };
-            std.debug.print("{c}", .{char});
-        }
-        std.debug.print("\n", .{});
-    }
-
-    // Create an image from the bitmap data
-    var image: rl.Image = undefined;
-    image.width = 50;
-    image.height = 36;
-    image.mipmaps = 1;
-    image.format = rl.PixelFormat.pixelformat_uncompressed_grayscale;
-    // const ptr: *anyopaque = bitmap.buffer().?.ptr;
-    image.data = @constCast(bitmap.buffer().?.ptr);
-
-    // Load the image into a texture
-    const texture: rl.Texture2D = rl.loadTextureFromImage(image);
-
-    // Main game loop
     while (!rl.windowShouldClose()) {
-        // Update
-        //----------------------------------------------------------------------------------
-
-        // Draw
-
         rl.beginDrawing();
         defer rl.endDrawing();
+
         rl.clearBackground(rl.Color.white);
 
-        rl.drawTexture(texture, 50, 25, rl.Color.white);
+        for (glyphInfos, glyphPositions) |info, pos| {
+            // this output looks ok-ish
+            // except that the offsets and advances in pos are quite large
+            // it's not clear to me how to interpret them
+            std.debug.print("codepoint: {d}\n", .{info.codepoint});
+            std.debug.print("{any}\n", .{pos});
+
+            // get a bitmap for the current glyph
+            try ft_face.loadGlyph(info.codepoint, .{ .render = true });
+            const glyph = ft_face.glyph();
+            const bm = glyph.bitmap();
+
+            std.debug.print("bitmap: {d}x{d}\n", .{ bm.width(), bm.rows() });
+
+            // create an image from the bitmap data
+            // this block is raylib specific
+            var image: rl.Image = undefined;
+            image.width = @intCast(bm.width());
+            image.height = @intCast(bm.rows());
+            image.mipmaps = 1;
+            image.format = rl.PixelFormat.pixelformat_uncompressed_grayscale;
+
+            // Do I need to create this copy?
+            // using the bitmap data directly requires a @constCast(bm.buffer().?.ptr) which looks like it can't possibly be correct.
+            const buffer = bm.buffer();
+            if (buffer == null) {
+                continue;
+            }
+            const bitmapBuffer = try allocator.dupe(u8, buffer.?);
+
+            // quite strange: If I uncomment this line, I get a double free error
+            // does raylib free some texture buffer itself?
+            // I'm not using raylib unloadImage or unloadTexture
+            // to me this looks like a big memory leak
+            // but: If I don't use the c_allocator, I get a "pointer wasn't allocated" error
+            // so I guess raylib is trying to free this buffer and if it was allocated with a different buffer, then it fails to do so
+            //defer allocator.free(bitmapBuffer);
+            image.data = bitmapBuffer.ptr;
+
+            // by default it's white text on black background
+            // I'd like to draw black on white
+            image.invert();
+
+            // to draw, we need to convert the image into a texture
+            const texture: rl.Texture2D = rl.loadTextureFromImage(image);
+            const center_x = 200;
+            const center_y = 100;
+            // ToDo: the positioning is off
+            // the offsets are much too large, and I've not yet figured out what to do with pos.advance
+            rl.drawTexture(texture, center_x + pos.x_offset, center_y + pos.y_offset, rl.Color.white);
+        }
     }
 }
